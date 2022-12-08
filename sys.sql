024cc3a6f25037ba3365f3a1d3501692b04037ba
Denes <denes.kosztyuk@gmail.com>
2022-12-08 11:16:34 +0000

    New DB with filled data, ver 01

--- sys.sql
+++ sys.sql
@@ -1,7 +1,30 @@
-CREATE DATABASE mobile_For_you;
+-- create a database that holds 2 managers, 5 employees, and 3 types of products
 
-CREATE TABLE employee(
-    emp_id INT,
-    
+CREATE DATABASE mobileForYou;
 
-)
+CREATE TABLE managers (
+    manager_id INT PRIMARY KEY,
+    manager_name VARCHAR(50),
+    manager_phone VARCHAR(50),
+    manager_email VARCHAR(50),
+    manager_address VARCHAR(50)
+);
+
+CREATE TABLE employees(
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(50),
+    employee_phone VARCHAR(50),
+    employee_email VARCHAR(50),
+    employee_manager_id INT,
+    FOREIGN KEY (employee_manager_id) REFERENCES managers(manager_id)
+);
+
+CREATE TABLE products(
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(50),
+    product_price INT,
+    product_type VARCHAR(50)
+);
+
+-- Path: sys.sql
+
