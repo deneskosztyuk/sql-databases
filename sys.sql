4cbcef7153dd961cc3f45dad98d04318ee340d47
Denes <denes.kosztyuk@gmail.com>
2022-12-08 13:53:36 +0000

    mobile_for-you-v0.2

--- sys.sql
+++ sys.sql
@@ -1,30 +1,33 @@
--- create a database that holds 2 managers, 5 employees, and 3 types of products
-
 CREATE DATABASE mobileForYou;
 
-CREATE TABLE managers (
-    manager_id INT PRIMARY KEY,
-    manager_name VARCHAR(50),
-    manager_phone VARCHAR(50),
-    manager_email VARCHAR(50),
-    manager_address VARCHAR(50)
-);
-
+-- creating an employee table
 CREATE TABLE employees(
     employee_id INT PRIMARY KEY,
-    employee_name VARCHAR(50),
-    employee_phone VARCHAR(50),
-    employee_email VARCHAR(50),
-    employee_manager_id INT,
+    employee_first_name VARCHAR(50),
+    employee_last_name VARCHAR(50),
+    employee_role VARCHAR(50),
     FOREIGN KEY (employee_manager_id) REFERENCES managers(manager_id)
 );
 
-CREATE TABLE products(
+-- creating a product table
+CREATE TABLE product(
     product_id INT PRIMARY KEY,
     product_name VARCHAR(50),
-    product_price INT,
-    product_type VARCHAR(50)
+    product_type VARCHAR(50),
+    product_price DECIMAL,
+    product_color VARCHAR(50),
+    product_OS VARCHAR(50),
+    product_CPU_speed INT,
+    product_RAM INT,
+    product_Battery_capacity INT,
 );
 
--- Path: sys.sql
+-- insert data into the employee table
+INSERT INTO employees VALUES (1, 'Dave', 'John', 'Manager', 1);
+INSERT INTO employees VALUES (2, 'Bruce', 'Banner', 'Manager', 1);
+INSERT INTO employees VALUES (3, 'Antony', 'Stark', 'Sales', 1);
+INSERT INTO employees VALUES (4, 'Simon', 'Riley', 'Sales', 1);
+INSERT INTO employees VALUES (5, 'Kanye', 'East', 'Sales', 1);
+INSERT INTO employees VALUES (6, 'Ben', 'Dover', 'Sales', 1);
+INSERT INTO employees VALUES (7, 'Peter', 'Parker', 'Sales', 1);
 
